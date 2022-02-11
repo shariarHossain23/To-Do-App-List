f648f5728ff430326a4d558ab090a8b692674ae0
shariarhossain23e <shariarhossain23@gamil.comm>
2022-02-11 17:07:41 +0600

    element added

---
+++ script.js
@@ -0,0 +1,36 @@
+let count = 0;
+
+document.getElementById("add-btn").addEventListener("click",function(){
+  count++
+    const inputField = document.getElementById("input-field").value;
+    if(inputField == ""){
+      alert("some text please")
+    }
+    else if(parseInt(inputField) > 0){
+        alert("plz input text")
+    }
+    else{
+      let newRow = document.createElement("tr");
+      newRow.innerHTML = `
+      <th scope="row">${count}</th>
+      <td>${inputField}</td>
+      <td>
+      <button class = "done btn btn-primary">Done </button>
+      <button class = "remove btn btn-danger mx-2">Remove </button>
+      </td>
+      `
+      const userList = document.getElementById("userList")
+      userList.appendChild(newRow)
+
+
+
+      document.getElementById("input-field").value = ""
+    }
+// delete element
+    const deleteBtn = document.getElementsByClassName("remove");
+ 
+})
+
+
+
+
